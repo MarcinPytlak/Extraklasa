af13fd9cc2d858e402972a1d1f4e90ce520c6967
MarcinPytlak <75089228+MarcinPytlak@users.noreply.github.com>
2021-12-03 12:21:37 +0100

    step3

--- src/components/layout/Table/ExtraklasaTable.js
+++ src/components/layout/Table/ExtraklasaTable.js
@@ -2,14 +2,21 @@ import React, {useState, useEffect} from 'react';
 import { Table } from 'react-bootstrap';
 import styles from './ExtraklasaTable.module.scss';
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 function ExtraklasaTable(){
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [urlAddress , setUrlAddress] = useState('https://api.sportradar.us/soccer/trial/v4/en/seasons/sr:season:77453/schedules.json?api_key=rs86utpab8axq6fkedt5yubf');
   //const [competitors, setCompetitors] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.sportradar.us/soccer/trial/v4/en/seasons/sr:season:77453/schedules.json?api_key=rs86utpab8axq6fkedt5yubf')
+    fetch(urlAddress)
       .then(res => res.json())
       .then(
         (result) => {
@@ -22,13 +29,13 @@ function ExtraklasaTable(){
           setError(error);
         }
       );
-  }, []);
+  }, );
   const isResult = (home, away) => {
     return home>=0 && away>= 0 
       ? 
       home + ':' + away 
       : 
-      'postponed due to COVID';
+      '--';
   };
 
   const isWinner =( team, tie, winner) =>{
@@ -46,9 +53,15 @@ function ExtraklasaTable(){
     if(it.status === 'closed'){
       return it.period_scores[0].home_score + ' : ' + it.period_scores[0].away_score;
     } else {
-      return 'postponed';
+      return '--';
     }
   };
+
+  const handleChange = (event) => {
+    setUrlAddress(event.target.value);
+    console.log(event);
+  };
+
   var count = 1;
 
   if (error) {
@@ -59,34 +72,54 @@ function ExtraklasaTable(){
     console.log(items);
     return (
       <div className={styles.container}>
-        <Table striped bordered hover>
-          <thead>
-            <tr className={styles.headings}>
-              <th>Match</th>
-              <th>Host</th>
-              <th>Guest</th>
-              <th>Final results</th>
-              <th>Match date</th>
-              <th>Half-Time score</th>
-              <th>Stadium Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map(item => 
-              <tr key={item.id} className={styles.rows}>
-            
-                <td>{count++}</td>
-                <td className={isWinner(item.sport_event.competitors[0].id, item.sport_event_status.match_tie, item.sport_event_status.winner_id)}>{item.sport_event.competitors[0].name}</td>
-                <td className={isWinner(item.sport_event.competitors[1].id, item.sport_event_status.match_tie, item.sport_event_status.winner_id)}>{item.sport_event.competitors[1].name}</td>
-                <td>{isResult(item.sport_event_status.home_score ,item.sport_event_status.away_score)}</td>
-                <td>{item.sport_event.start_time.substr(0,10)}</td>
-                <td>{breakScore(item.sport_event_status)}</td>
-                <td>{item.sport_event.venue.name}</td>
+        <div className={styles.dropdown}>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Select season</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={urlAddress}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={'https://api.sportradar.us/soccer/trial/v4/en/seasons/sr:season:77453/schedules.json?api_key=rs86utpab8axq6fkedt5yubf'}>Ekstraklasa 20/20</MenuItem>
+                <MenuItem value={'https://api.sportradar.us/soccer/trial/v4/en/seasons/sr:season:67233/schedules.json?api_key=rs86utpab8axq6fkedt5yubf'}>Ekstraklasa 19/20</MenuItem>
+                <MenuItem value={'https://api.sportradar.us/soccer/trial/v4/en/seasons/sr:season:84320/schedules.json?api_key=rs86utpab8axq6fkedt5yubf'}>Ekstraklasa 21/22</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+        <div className={styles.table}>
+          <Table striped bordered hover>
+            <thead>
+              <tr className={styles.headings}>
+                <th>Match</th>
+                <th>Host</th>
+                <th>Guest</th>
+                <th>Final results</th>
+                <th>Match date</th>
+                <th>Half-Time score</th>
+                <th>Stadium Name</th>
               </tr>
-            )}
+            </thead>
+            <tbody>
+              {items.map(item => 
+                <tr key={item.id} className={styles.rows}>
+            
+                  <td>{count++}</td>
+                  <td className={isWinner(item.sport_event.competitors[0].id, item.sport_event_status.match_tie, item.sport_event_status.winner_id)}>{item.sport_event.competitors[0].name}</td>
+                  <td className={isWinner(item.sport_event.competitors[1].id, item.sport_event_status.match_tie, item.sport_event_status.winner_id)}>{item.sport_event.competitors[1].name}</td>
+                  <td>{isResult(item.sport_event_status.home_score ,item.sport_event_status.away_score)}</td>
+                  <td>{item.sport_event.start_time.substr(0,10)}</td>
+                  <td>{breakScore(item.sport_event_status)}</td>
+                  <td>{item.sport_event.venue.name}</td>
+                </tr>
+              )}
           
-          </tbody>
-        </Table>
+            </tbody>
+          </Table>
+        </div>
       </div>  
     );
   }
